c10f19cff229027004595fa5b905ce5d09189ded
Christopher Baptiste <chrisbaptiste83@gmail.com>
2019-08-31 13:23:04 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -114,7 +114,7 @@ def initialize(path = "./db/mp3s")
     #if (input > 0) && (input <= list_of_songs.size)
     #if (input >= 1) && (input <= list_of_songs.size)
     if (1..Song.all.length).include?(input)
-      song = list_of_songs[input+2]
+      song = list_of_songs[input.to_i]
       puts "Playing #{song.name} by #{song.artist.name}"
     end
   end
